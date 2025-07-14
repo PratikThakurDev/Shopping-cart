af21ebdee6b8ddda90fc5ad319d82a4e55a617fc
Pratik <149695885+PratikThakurDev@users.noreply.github.com>
2025-07-14 11:23:33 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+    return (
+        <div>
+            <center>
+                <h1>Vite + React Boilerplate</h1>
+            </center>
+        </div>
+    );
+}
+
+export default App;
